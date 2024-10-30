fba0b01630b47397364cb16210fc224f65f2d3e1
nehanaseem46 <nehanaseem20@gmail.com>
2024-10-30 21:22:56 +0500

    Add files via upload
    assignment 3

---
+++ neha naseem.py
@@ -0,0 +1,39 @@
+number1=3
+number2=3
+number3=3
+
+#calculate sum
+sum=number1+number2+number3
+print("sum:",sum )
+
+#calculate difference 
+difference=number1-number2-number3
+print("difference:", difference)
+
+#calculate product
+product=number1*number2*number3
+print ("product:", product)
+
+#calculate average
+average=number1+number2+number3
+print ("average:", average)
+
+#area of triangle
+breadth=2
+height=9
+area=0.5*breadth*height
+print("triangle area",area)
+#area of square
+side=6
+area=side*side
+print("square area", area)
+#area of circle
+pi=3.14
+radius=7
+area=pi*radius*radius
+print("circle area",area)
+#rectangle area 
+length=2
+width=5
+area=length*width
+print("rectangle area",area)
